f0a5e0fc00b5b81e2ecaf0b299af239b4d0b6c75
limjiminn <jiaezzan@naver.com>
2022-01-06 22:01:13 +0900

    참가 인원수 제한하기

--- app/src/main/java/com/example/android_test/MainActivity.kt
+++ app/src/main/java/com/example/android_test/MainActivity.kt
@@ -6,62 +6,65 @@ import android.widget.Button
 import android.widget.TextView
 import java.lang.Math.abs
 import java.util.*
+import kotlin.concurrent.timer
 
 class MainActivity : AppCompatActivity() {
+    var p_num = 3
+    var k = 1
+    val point_list = mutableListOf<Float>()
     //메인함수
     fun main(){
         //xml 파일의 레이아웃을 불러와라
         setContentView(R.layout.activity_main)
 
+        //1.hell 텍스트를 "안녕"으로 변환
+        //2.시간 표시 로직 적용하기
+        //3.목표시간 랜덤으로 정하기
+        var timerTask: Timer? = null  //널러블? //timer는 변수가 아닌 함수
+        var stage = 1
+        var sec : Int = 0
+        val tv:TextView = findViewById(R.id.tv_random)
+        val tv_t:TextView = findViewById(R.id.tv_timer)
+        val tv_p:TextView = findViewById(R.id.tv_point)
+        val tv_people:TextView = findViewById(R.id.tv_people)
+        val btn:Button = findViewById(R.id.btn_main)
+        val random_box = Random()  //랜덤함수 사용하기
+        val num = random_box.nextInt(1001) // 0부터 1000까지
 
+        tv.text = (num.toFloat()/100).toString()
+        btn.text = "시작"
+        tv_people.text ="참가자 $k"
+        btn.setOnClickListener {
+            stage++
+            if (stage == 2) {
+                timerTask = kotlin.concurrent.timer(period = 10) {
+                    sec++
+                    runOnUiThread {
+                        tv_t.text = (sec.toFloat() / 100).toString()
+                    }
+                }
+                btn.text = "정지"
+            } else if (stage == 3) {
+                timerTask?.cancel()
+                val point = abs(sec - num).toFloat() / 100
+                point_list.add(point)
+                tv_p.text = point.toString()
+                btn.text = "다음"
+                stage = 0
+            } else if (stage == 1) {
+                if (k < p_num) {
+                    k++
+                    main()
+                } else {
+                    println(point_list)
+                }
 
+            }
+        }
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         main()
-
-        //1.hell 텍스트를 "안녕"으로 변환
-        //2.시간 표시 로직 적용하기
-        var sec : Int = 0
-        var isRunning = false
-        val tv:TextView = findViewById(R.id.tv_random)
-        val tv_t:TextView = findViewById(R.id.tv_timer)
-        val tv_p:TextView = findViewById(R.id.tv_point)
-        val btn:Button = findViewById(R.id.btn_main)
-        //널러블? //timer는 변수가 아닌 함수
-        var timerTask: Timer? = null
-        //3.목표시간 랜덤으로 정하기
-            //랜덤함수 사용하기
-            val random_box = Random()
-//            val num = random_box.nextInt(11) //0부터 10까지
-            val num = random_box.nextInt(1001)
-                        //소수로 만들어서 소수점 2자리 까지 랜덤
-            tv.text = ((num.toFloat())/100).toString()
-
-
-         btn.setOnClickListener {
-             isRunning = !isRunning
-             //러닝이 true면 돌고 아니면 스탑
-             if(isRunning == true){
-                 //1000을 넣어야 1초마다 함수가 돌아간다
-                 timerTask = kotlin.concurrent.timer(period = 10){
-                     sec++
-                     //실시간으로 화면이 바뀌는것 처리하려면 사용
-                     runOnUiThread {
-                         //int를 string 으로 변환
-                                //소수점 2까지 나오게하기
-                         tv_t.text = (sec.toFloat()/100).toString()
-                     }
-                 }
-             }else {
-                 //타이머 멈추기
-                 timerTask?.cancel()
-                        //abs : 절댓값
-                 val point = abs(sec-num).toFloat()/100
-                 tv_p.text = point.toString()
-             }
-
-        }
 
     }
 }
