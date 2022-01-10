99996db5f964346112f6703895acb73b6e0e07d9
limjiminn <jiaezzan@naver.com>
2022-01-10 22:43:23 +0900

    블라인드 모드 구현하기

--- app/src/main/java/com/example/android_test/MainActivity.kt
+++ app/src/main/java/com/example/android_test/MainActivity.kt
@@ -11,6 +11,7 @@ class MainActivity : AppCompatActivity() {
     var p_num = 3
     var k = 1
     val point_list = mutableListOf<Float>()
+    var isBlind = false
         //시작페이지
     fun start(){
         setContentView(R.layout.activity_start)
@@ -18,6 +19,17 @@ class MainActivity : AppCompatActivity() {
             val btn_minus:Button = findViewById(R.id.btn_minus)
             val btn_plus:Button = findViewById(R.id.btn_plus)
             val btn_start:Button = findViewById(R.id.btn_start)
+            val btn_blind:Button = findViewById(R.id.btn_blind)
+
+            btn_blind.setOnClickListener {
+                isBlind = !isBlind
+                if(isBlind == true){
+                    btn_blind.text = "Blind 모드 ON"
+                }else{
+                    btn_blind.text = "Blind 모드 OFF"
+                }
+            }
+
             tv_pnum.text = p_num.toString()
             btn_minus.setOnClickListener {
                 p_num--
@@ -51,24 +63,38 @@ class MainActivity : AppCompatActivity() {
         val tv_t:TextView = findViewById(R.id.tv_pnum)
         val tv_p:TextView = findViewById(R.id.tv_point)
         val tv_people:TextView = findViewById(R.id.tv_people)
-        val btn:Button = findViewById(R.id.btn_start)
+        val btn:Button = findViewById(R.id.btn_blind)
+        val btn_i:Button = findViewById(R.id.btn_i)
         val random_box = Random()  //랜덤함수 사용하기
         val num = random_box.nextInt(1001) // 0부터 1000까지
 
         tv.text = (num.toFloat()/100).toString()
         btn.text = "시작"
         tv_people.text ="참가자 $k"
+
+        btn_i.setOnClickListener {
+            point_list.clear()
+            k = 1
+            start()
+        }
+
         btn.setOnClickListener {
             stage++
             if (stage == 2) {
                 timerTask = kotlin.concurrent.timer(period = 10) {
                     sec++
                     runOnUiThread {
-                        tv_t.text = (sec.toFloat() / 100).toString()
+                        if(isBlind == false){
+                            tv_t.text = (sec.toFloat() / 100).toString()
+                        }else if(isBlind == true && stage == 2){
+                            tv_t.text = "???"
+                        }
+
                     }
                 }
                 btn.text = "정지"
             } else if (stage == 3) {
+                tv_t.text = (sec.toFloat() / 100).toString()
                 timerTask?.cancel()
                 val point = abs(sec - num).toFloat() / 100
                 point_list.add(point)
